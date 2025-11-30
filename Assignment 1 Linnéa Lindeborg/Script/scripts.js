25f93bb0d41e26180014dea4397dd66991cbfdaa
linnealindeborg-hue <linnea.lindeborg@gritacademy.se>
2025-11-30 14:16:33 +0100

    First assigntment Linnéa Lindeborg
    learning to code

---
+++ Assignment 1 Linnéa Lindeborg/Script/scripts.js
@@ -0,0 +1,15 @@
+let foo=0
+let bar=0
+
+document.getElementById("divide").onclick = () => console.log("divide");
+ 
+
+let result = foo/bar;
+
+
+console.log(foo/bar);
+
+
+if (Number.isNaN(result)) {
+    alert("you cant divide us!")
+}
